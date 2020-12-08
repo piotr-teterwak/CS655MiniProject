db44f4230bda629bee37e869be6f7bc65af00c57
piotrt <piotrt@balancer.miniprojectimgclass.ch-geni-net.instageni.colorado.edu>
2020-12-08 13:56:21 -0700

    v1 websiteOP

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import requests
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def submit_page():
+    return render_template('index.html')
 
 @app.route('/submit', methods=["POST","GET"])
 def hello_world():
-    app.logger.info("SUCCESS")
-    return 'Hello, World!'
+    image = request.files.get("image")
+    payload = {"image":image}
+    requests.post('http://192.12.245.174:5000/predict', files=payload).json()
+    return r
