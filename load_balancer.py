c4f564e4ce66ffa44402872cb437a2664352b96d
piotrt <piotrt@balancer.miniprojectimgclass.ch-geni-net.instageni.colorado.edu>
2020-12-09 14:27:35 -0700

    Change name of hello to load_balancer

--- load_balancer.py
+++ load_balancer.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template,request
 import requests
 import random
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 #servers = ['192.12.245.174','192.12.245.175','192.12.245.176','192.12.245.177']
 servers = ['10.10.1.1','10.10.2.1','10.10.3.1','10.10.4.1']
 
-@app.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def submit_page():
     return render_template('index.html')
 
-@app.route('/submit', methods=["POST","GET"])
+@application.route('/submit', methods=["POST","GET"])
 def hello_world():
     image = request.files.get("image")
     payload = {"image":image}
@@ -19,7 +19,10 @@ def hello_world():
         num_servers = int(request.form.get("number_backend_servers")) - 1
     else:
         num_servers = len(servers)-1
-    server_idx = random.randint(0,len(servers)-1)
+    server_idx = random.randint(0,num_servers)
     r = requests.post('http://{}:5000/predict'.format(servers[server_idx]), files=payload).text
     return r
     #return "Hello, world!"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000, threaded=True)
