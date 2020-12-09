8e5381dc780dc4bd34daf0e9e1b6626b19bae2e9
piotrt <piotrt@client.miniprojectimgclass.ch-geni-net.instageni.colorado.edu>
2020-12-09 10:01:42 -0700

    Better install script

--- install_script.sh
+++ install_script.sh
@@ -1,3 +1,5 @@
-bash ~/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+bash ~/CS655MiniProject/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+eval "$(/users/$USER/miniconda/bin/conda shell.bash hook)"
+conda init
+conda install --file requirements.txt -y
 source ~/.bashrc
-conda install --file requirements.txt
