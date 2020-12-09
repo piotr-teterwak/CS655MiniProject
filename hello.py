ecb60258c72e06aa54d438e651dd31e235e7fcfe
piotrt <piotrt@balancer.miniprojectimgclass.ch-geni-net.instageni.colorado.edu>
2020-12-09 11:13:36 -0700

    Added servers.

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import random
 app = Flask(__name__)
 
 
-servers = ['192.12.245.174']
+servers = ['192.12.245.174','192.12.245.175','192.12.245.176','192.12.245.177']
 
 @app.route('/', methods=['GET'])
 def submit_page():
@@ -19,6 +19,6 @@ def hello_world():
     else:
         num_servers = len(servers)-1
     server_idx = random.randint(0,len(servers)-1)
-    #r = requests.post('http://{}:5000/predict'.format(servers[server_idx]), files=payload).text
-    #return r
-    return "Hello, world!"
+    r = requests.post('http://{}:5000/predict'.format(servers[server_idx]), files=payload).text
+    return r
+    #return "Hello, world!"
