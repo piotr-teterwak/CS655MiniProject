1d1adcec57e66aeb149f327eb325f12db68bbd9e
Piotr Teterwak <piotr.teterwak@gmail.com>
2020-12-09 20:23:17 -0500

    Pull source bashrc out

--- install_script.sh
+++ install_script.sh
@@ -2,4 +2,3 @@ bash ~/CS655MiniProject/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 eval "$(/users/$USER/miniconda/bin/conda shell.bash hook)"
 conda init
 conda install --file requirements.txt -y
-source ~/.bashrc
