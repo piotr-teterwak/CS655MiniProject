8bd7beee75363045cf18624f4a9b67b6ea56bab8
Piotr Teterwak <piotr.teterwak@gmail.com>
2020-12-09 21:41:51 -0500

    Cleanup

--- test_script.py
+++ test_script.py
@@ -5,17 +5,7 @@ from time import sleep
 import time
 import argparse
 
-#parser = argparse.ArgumentParser()
-
-#parser.add_argument("average_wait_interval", help="Average wait between simulated traffic requests", type=float)
-#parser.add_argument("measurement_frequency", help="Measure rtt every measure_frequency requests", type=int)
-#parser.add_argument("number_backend_servers", help="How many backend servers to use", type=int)
-#parser.add_argument("loop_length", help="How many requests to send", type=int)
-#parser.add_argument("image_path", help="Test image path", type=str)
-#args = parser.parse_args()
-
 REST_API_URL = "http://10.10.5.1:5000/submit"
-#REST_API_URL = "http://localhost:5000/submit"
 
 pool = Pool(1000)
 s = requests.Session()
